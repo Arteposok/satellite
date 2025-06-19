d0d6c01145e044dc17787fc446be5afb5d97baf3
Arteposok <aeposokhov@gmail.com>
2025-06-19 14:12:48 +0400

    added graphing

--- graph.py
+++ graph.py
@@ -1,7 +1,8 @@
 import dearpygui.dearpygui as dpg
 import socket as sk
-import json
 import numpy as np
+import threading
+import json
 
 dpg.create_context()
 dpg.create_viewport(title="graph", width=500, height=500)
@@ -14,23 +15,26 @@ lux = np.ones(100).tolist()
 def get_data():
     global temp, lux, x_base
     with sk.socket(sk.AF_INET, sk.SOCK_STREAM) as sock:
-        sock.connect((input(), 8888))
+        sock.connect(("192.168.4.1", 8888))
         while True:
             data = json.loads(sock.recv(1024).decode())
             temp.append(data[0])
             lux.append(data[1])
             x_base.append(x_base[-1] + 1)
-            temp = temp[-100:]
-            lux = temp[-100:]
-            x_base = temp[-100:]
+            temp = temp[-20:]
+            lux = lux[-20:]
+            x_base = x_base[-20:]
             dpg.set_value("temp", (x_base, temp))
             dpg.set_value("lux", (x_base, lux))
+            dpg.set_axis_limits("x_axis", x_base[0], x_base[-1])
 
+
+threading.Thread(target=get_data, daemon=True).start()
 
 with dpg.window(tag="main"):
     with dpg.plot(label="Value graphs", height=400, width=400):
         dpg.add_plot_legend()
-        dpg.add_plot_axis(dpg.mvXAxis, label="x")
+        dpg.add_plot_axis(dpg.mvXAxis, label="x", tag="x_axis")
         dpg.add_plot_axis(dpg.mvYAxis, label="y", tag="y_axis")
         dpg.add_line_series(
             x_base, temp, label="Temperature", parent="y_axis", tag="temp"
