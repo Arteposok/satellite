32751d10c1eb6cf6d13c9a40cb23036144f169be
Arteposok <aeposokhov@gmail.com>
2025-06-19 09:26:22 +0400

    none

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import socket
 from rich.panel import Panel
 from rich.live import Live
+from rich.console import Console
 import json
+
+c = Console()
 
 def test():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((input("IP adress > "), 8888))
-        with Live(Panel("Temp, Lux"), refresh_per_second=2) as live:
+        s.connect((c.input("[bold]IP adress[/bold] > "), 8888))
+        with Live(Panel("Temp, Lux", title="Data"), refresh_per_second=2) as live:
             while True:
                 x = s.recv(1024).decode()
                 obj = json.loads(x)
-                live.update(Panel(f"{obj[0]} C \t {obj[1]} ~lux"))
+                live.update(Panel(f"{obj[0]} C \t {obj[1]} ~lux", title="Data"))
 
 
 test()
